c01811d4f0a17185a19daa2b952a01a3d383d776
Reyner Veliz <reyner@reynerveliz.com>
2023-10-29 15:37:57 -0600

    Enumerate show

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ while True:
     if user_action == "add":
         todo = input("Enter a todo: ")
         todos.append(todo)
+
     elif user_action == "show":
-        for item in todos:
-            print(item)
+        for index, item in enumerate(todos):
+            print(f"{index + 1} - {item}")
+
     elif user_action == "edit":
         number = int(input("Number of the todo to edit: "))
         number = number - 1
         new_todo = input("Enter new todo: ")
         todos[number] = new_todo
+
     elif user_action == "exit":
         break
+
     else:
         print("Could not understand command")
