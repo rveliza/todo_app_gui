8252b37f3ef715d869de7f8b5b05085e6fdf5e18
Reyner Veliz <reyner@reynerveliz.com>
2023-10-29 15:18:01 -0600

    Store input repeatedly

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 user_prompt = "Enter a todo: "
 
+todos = []
 
 while True:
     todo = input(user_prompt)
-    print(todo)
-    print("Next...")
+    print(todo.capitalize())
+    todos.append(todo)
