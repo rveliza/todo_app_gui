cf2073de2bb52e93d1070915a6d274df46f36a8b
Reyner Veliz <reyner@reynerveliz.com>
2023-10-29 20:54:01 -0600

    Implement function to avoid repetitive code

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-user_prompt = "Type add, show, edit, complete or exit: "
+def get_todos():
+    with open("todos.txt", "r") as file:
+        todos = file.readlines()
+    return todos
 
+
+user_prompt = "Type add, show, edit, complete or exit: "
 
 while True:
     user_action = input(user_prompt)
@@ -7,50 +12,39 @@ while True:
     
     if user_action.startswith("add"):
         todo = user_action[4:]
-        
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
-
+        todos = get_todos()
         todos.append(todo + "\n")
 
         with open("todos.txt", "w") as file:
             file.writelines(todos)
 
     elif user_action.startswith("show"):
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
-
+        todos = get_todos()
         new_todos = [item.strip("\n") for item in todos]
 
         for index, item in enumerate(new_todos):
             print(f"{index + 1} - {item}")
 
     elif user_action.startswith("edit"):
+
         try:
             number = int(user_action[5:])
             number = number - 1
-
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
-
+            todos = get_todos()
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
 
             with open("todos.txt", "w") as file:
                 file.writelines(todos)
+                
         except ValueError:
             print("Your command is not valid")
     
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
-
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
-
-
+            todos = get_todos()
             removed_todo = todos.pop(number - 1).strip("\n")
-
 
             with open("todos.txt", "w") as file:
                 file.writelines(todos)
