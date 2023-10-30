37db5c2e3eb42dbad09009f343118aacc0feab76
Reyner Veliz <reyner@reynerveliz.com>
2023-10-29 22:13:46 -0600

    Create basic GUI without interactions, and add a todo button

---
+++ gui.py
@@ -0,0 +1,26 @@
+import functions
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter a todo", key="todo")
+add_button = sg.Button("Add")
+
+window = sg.Window("My To-Do App", 
+                   layout=[[label], [input_box, add_button]], 
+                   font=("Helvetica", 20))
+
+
+while True:
+    event, values = window.read()
+    print(event) # Add
+    print(values) # ('Add', {'todo': 'hi'})
+    if event == "Add":
+        todos = functions.get_todos()
+        new_todo = values["todo"] + "\n"
+        todos.append(new_todo)
+        functions.write_todos(todos)
+
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
