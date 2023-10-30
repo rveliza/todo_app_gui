4ee51e09e9338b88c2bf3036fb72950b7f06f2a1
Reyner Veliz <reyner@reynerveliz.com>
2023-10-29 21:34:53 -0600

    Add a "date" feature

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """
     Read a text file and return the list of to-do items
     """
@@ -6,7 +8,7 @@ def get_todos(filepath="todos.txt"):
         todos = file.readlines()
     return todos
 
-def write_todos(todos, filepath="todos.txt"):
+def write_todos(todos, filepath=FILEPATH):
     """
     Write the to-do items to the text file
     """
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from functions import get_todos, write_todos
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is now ", now)
 
 user_prompt = "Type add, show, edit, complete or exit: "
 
